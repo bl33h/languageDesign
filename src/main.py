2f2ac735ba595390a798bd242b9bb4a53d7429b6
bl33h <per21371@uvg.edu.gt>
2024-02-08 23:28:15 -0600

    thompson now works when it's called from the main, the subsets method  works fine, it still crashes during extreme cases

--- src/main.py
+++ src/main.py
@@ -4,3 +4,24 @@
 #Version: I
 #Creation: 06/02/2024
 #Last modification: 06/02/2024
+
+from shuntingYard import shuntingYard   
+from thompson import thompson
+from subsets import DFAFromNFA
+
+if __name__ == "__main__":
+    regex_input = input("enter the regular expression: ")
+    postfix = shuntingYard(regex_input)
+    nfa_builder = thompson(postfix)
+    nfa = nfa_builder.nfa
+    
+    nfa.display('nfa_graph', 'NFA Visualization')
+
+    string_w = input("Enter the string w: ")
+    if nfa_builder.analyzeNFA(string_w):
+        print(f"the string'{string_w}' w∈L(r)")
+    else:
+        print(f"the string'{string_w}' w∉L(r)")
+
+    dfaFromNfa = DFAFromNFA(nfa)
+    dfaFromNfa.displayDFA('dfa_graph', 'DFA Visualization')
--- src/subsets.py
+++ src/subsets.py
@@ -5,69 +5,74 @@
 #Creation: 07/02/2024
 #Last modification: 08/02/2024
 
-from collections import defaultdict, deque
-from thompson import Thompson
-from thompson import NFA
+from collections import defaultdict
+from graphviz import Digraph
+from thompson import *
+from shuntingYard import *
 
-class DFA:
-    def __init__(self):
-        self.states = []
-        self.symbol_map = defaultdict(set)
-        self.accept_states = set()
-        self.initial_state = None
-        self.transition_table = defaultdict(dict)
+class DFAFromNFA:
+    def __init__(self, nfa):
+        self.nfa = nfa
+        self.dfa_states = {}
+        self.dfa_start_state = None
+        self.dfa_accept_states = []
+        self.dfa_transitions = defaultdict(lambda: defaultdict(set))
+        self.construct_dfa()
 
-    def addState(self, state, is_accept=False):
-        if state not in self.states:
-            self.states.append(state)
-            if is_accept:
-                self.accept_states.add(state)
+    def epsilonClosure(self, states):
+        closure = set(states)
+        stack = list(states)
+        while stack:
+            state = stack.pop()
+            if state in self.nfa.transitions:
+                for to_state in self.nfa.transitions[state]:
+                    if epsilon in self.nfa.transitions[state][to_state] and to_state not in closure:
+                        closure.add(to_state)
+                        stack.append(to_state)
+        return frozenset(closure)
 
-    def set_initial_state(self, state):
-        self.initial_state = state
+    def move(self, states, symbol):
+        next_states = set()
+        for state in states:
+            if state in self.nfa.transitions:
+                for to_state, symbols in self.nfa.transitions[state].items():
+                    if symbol in symbols:
+                        next_states.add(to_state)
+        return frozenset(next_states)
 
-    def add_transition(self, from_state, to_state, symbol):
-        self.symbol_map[symbol].add((from_state, to_state))
-        self.transition_table[from_state][symbol] = to_state
+    def construct_dfa(self):
+        self.dfa_start_state = self.epsilonClosure([self.nfa.initialState])
+        queue = [self.dfa_start_state]
+        self.dfa_states[self.dfa_start_state] = 0  # Mapping DFA state to an index
 
-def epsilonClosure(thompson, states):
-    closure = set(states)
-    stack = list(states)
-    
-    while stack:
-        state = stack.pop()
-        for to_state in thompson.epsilon_transitions(state):
-            if to_state not in closure:
-                closure.add(to_state)
-                stack.append(to_state)
-    
-    return closure
+        while queue:
+            current_dfa_state = queue.pop(0)
+            for symbol in self.nfa.symbols:
+                if symbol != epsilon:
+                    next_state = self.epsilonClosure(self.move(current_dfa_state, symbol))
+                    if next_state not in self.dfa_states:
+                        self.dfa_states[next_state] = len(self.dfa_states)
+                        queue.append(next_state)
+                    self.dfa_transitions[current_dfa_state][symbol].add(next_state)
+                    if any(s in self.nfa.acceptStates for s in next_state):
+                        self.dfa_accept_states.append(next_state)
 
-def move(thompson, states, symbol):
-    new_states = set()
-    for state in states:
-        new_states |= thompson.transition(state, symbol)
-    return new_states
+    def displayDFA(self, fileName='dfa.gv', projectName='deterministic_finite_state_machine'):
+        outputDir = 'dfaOutput'
+        os.makedirs(outputDir, exist_ok=True)
+        dotFilePath = os.path.join(outputDir, fileName)
+        graph = Digraph(projectName, filename=fileName, format='png')
+        graph.attr(rankdir='LR')
+        graph.attr('node', shape='doublecircle')
+        for accept_state in self.dfa_accept_states:
+            graph.node('s' + str(self.dfa_states[accept_state]))
 
-def NFAtoDFA(thompson):
-    dfa = DFA()
-    initial_closure = epsilonClosure(thompson, [thompson.initial_state])
-    unmarked_states = deque([frozenset(initial_closure)])
-    state_names = {frozenset(initial_closure): 0}
-    dfa.set_initial_state(0)
-    
-    while unmarked_states:
-        current_states = unmarked_states.popleft()
-        for symbol in thompson.symbols - {thompson.epsilon}:
-            move_closure = epsilonClosure(thompson, move(thompson, current_states, symbol))
-            if not move_closure:
-                continue
-            move_closure_frozenset = frozenset(move_closure)
-            if move_closure_frozenset not in state_names:
-                state_names[move_closure_frozenset] = len(state_names)
-                unmarked_states.append(move_closure_frozenset)
-                if thompson.accept_states & move_closure:
-                    dfa.addState(state_names[move_closure_frozenset], is_accept=True)
-            dfa.add_transition(state_names[current_states], state_names[move_closure_frozenset], symbol)
-    
-    return dfa
+        graph.attr('node', shape='circle')
+        for from_state, transitions in self.dfa_transitions.items():
+            for symbol, to_states in transitions.items():
+                for to_state in to_states:
+                    graph.edge('s' + str(self.dfa_states[from_state]), 's' + str(self.dfa_states[to_state]), label=symbol)
+
+        graph.attr('node', shape='point')
+        graph.edge('', 's' + str(self.dfa_states[self.dfa_start_state]))
+        graph.render(filename=dotFilePath, directory=outputDir, view=False)
