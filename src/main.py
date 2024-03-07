26ce5912f70e8b05fd32b361b6a5cbec37e51814
bl33h <per21371@uvg.edu.gt>
2024-03-07 00:36:00 -0600

    bugs fixed

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def main():
     etiWelcome2 = ct.CTkLabel(win, text="Below you will find the first two phases to build a compiler: Lexical and syntactic analyzer.", font=("Arial", 11, "normal"), text_color="#2D3041")
     etiWelcome3 = ct.CTkLabel(win, text="For both parts, the respective algorithms have been implemented correctly.", font=("Arial", 11, "normal"), text_color="#2D3041")
     etiWelcome2.pack(pady=0)
-    etiWelcome3.pack(pady=0)
+    etiWelcome3.pack(pady=7)
 
     buttonLexicalAnalyzer = ct.CTkButton(win, text="Lexical analyzer", fg_color="#797fa2", hover_color="#454B70", command=showLexicalAnalyzerUI)
     buttonLexicalAnalyzer.pack(pady=7)
@@ -31,7 +31,10 @@ def main():
     buttonSyntaxAnalyzer = ct.CTkButton(win, text="Syntax analyzer", fg_color="#797fa2", hover_color="#454B70", command=showSyntaxAnalyzerUI)
     buttonSyntaxAnalyzer.pack(pady=7)
 
-    win.geometry("500x200")
+    authorC = ct.CTkLabel(win, text="By Sara Echeverría Copyright (C), 2024-2025, bl33h", font=("Arial", 8, "normal"), text_color="#2D3041")
+    authorC.pack(pady=7)
+    
+    win.geometry("500x250")
     win.mainloop()
 
 def showLexicalAnalyzerUI():
